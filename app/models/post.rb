088c5a5483abc756864e9a266252946aba727004
bbbtttiii <ben.iburg@gmail.com>
2020-09-14 08:43:01 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+    CLICKBAIT_KEYWORDS = [/won't believe/i, /secret/i, /top[0-9]*/i, /guess/i]
+
+    def clickbait?
+        if CLICKBAIT_KEYWORDS.none? { |x| x.match title }
+            errors.add(:title, "clickbait")
+        end
+    end
+
 end
